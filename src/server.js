9f437aa864c1a9b50b8dd5c82c66ce9dc3a1843d
mrizkyrn <mrizkyr2411@gmail.com>
2023-12-10 14:52:40 +0700

    add postgressql for notes service

--- src/server.js
+++ src/server.js
@@ -1,31 +1,34 @@
+const dotenv = require('dotenv');
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NotesService');
 const NotesValidator = require('./validator/notes');
 
+dotenv.config();
+
 const init = async () => {
    const notesService = new NotesService();
 
-    const server = Hapi.server({
-        port: 3000,
-        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-        routes: {
-          cors: {
-              origin: ['*'],
-          },
-        },
-    });
+   const server = Hapi.server({
+      port: process.env.PORT,
+      host: process.env.HOST,
+      routes: {
+         cors: {
+            origin: ['*'],
+         },
+      },
+   });
 
-    await server.register({
+   await server.register({
       plugin: notes,
       options: {
-        service: notesService,
-        validator: NotesValidator,
+         service: notesService,
+         validator: NotesValidator,
       },
-    });
+   });
 
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
+   await server.start();
+   console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
